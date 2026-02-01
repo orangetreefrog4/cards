4d75a9a29153d4f824f1e84757cfc96b7834445a
Liam Owen <123574555+orangetreefrog4@users.noreply.github.com>
2026-02-01 19:21:57 +0000

    Add files via upload
    Got the game in a playable state. I forgot to commit regularly today, though. :/ It's very messy, sorry if anyone's trying to look at my code. Most of the logic is in the script for the node "game" in the scene "game". I have, like 5 different functions that trigger each other just to deal one card, even going between different nodes (godot objects) and back to call to update scores and such. It's not very readable, but I'm happy with the progress i've been making and very happy that I'm learning Godot and game dev isn't actually as hard as I thought it would be. Thank you Hack Club! :)

--- cards/assets/globals.gd
+++ cards/assets/globals.gd
@@ -1,9 +1,12 @@
 extends Node
 
-var player_cards = []
+var player_cards = [0,0,0,0,0]
 var player_dealt_amount = 0
-var opponent_cards = []
+var opponent_cards = [0,0,0,0,0]
 var opponent_dealt_amount = 0
 var player_score = 0
 var opponent_score = 0
+var win_reason = ""
 var round = 0
+
+var dev_mode = false
--- cards/scenes/game/game.gd
+++ cards/scenes/game/game.gd
@@ -1,83 +1,163 @@
 extends Node2D
 
 var g = globals
+var dev = globals.dev_mode
 
-signal set_card1(value)
-signal set_card2(value)
-signal set_card3(value)
-signal set_card4(value)
-signal set_card5(value)
-signal opponent_card1(value)
-signal opponent_card2(value)
-signal opponent_card3(value)
-signal opponent_card4(value)
-signal opponent_card5(value)
+var opponent_decision_chances = [0.9,0.9,0.9,0.9,0.8,0.8,0.8,0.7,0.7,0.7,0.6,0.6,0.5,0.5,0.4,0.4,0.2,0.2,0.1,0.1]
+
+signal update_game()
+signal dev_mode_enabled()
 signal set_score(player_value, opponent_value)
 
 var card_value = 0
 
 func _ready() -> void:
-	pass
+	get_node("Winner").set("visible", false)
+	get_node("Winner/PlayerWin").set("visible", false)
+	get_node("Winner/OpponentWin").set("visible", false)
 
 func deal_card() -> int:
-	return (randi() % 2 + 1)
+	return (randi() % 10 + 1)
 
 func initial_deal_player() -> void:
-	for x in range(2) :
-		card_value = deal_card()
-		print("Card no {x} will be {card_value}.")
-		g.player_cards.append(card_value)
-		print("Card no {x} is added.")
-	set_card1.emit(g.player_cards[0])
+	card_value = deal_card()
+	g.player_cards[0] = card_value
 	g.player_dealt_amount += 1
-	set_card2.emit(g.player_cards[1])
+	card_value = deal_card()
+	g.player_cards[1] = card_value
 	g.player_dealt_amount += 1
+	update_game.emit()
 	get_node("UI/PostDealt").set("visible", true)
 
 func initial_deal_opponent() -> void:
-	for x in range(2) :
-		card_value = deal_card()
-		print("Card no {x} will be {card_value}.")
-		g.opponent_cards.append(card_value)
-		print("Card no {x} is added.")
-	opponent_card1.emit(g.opponent_cards[0])
+	card_value = deal_card()
+	g.opponent_cards[0] = card_value
 	g.opponent_dealt_amount += 1
-	opponent_card2.emit(g.opponent_cards[1])
+	card_value = deal_card()
+	g.opponent_cards[1] = card_value
 	g.opponent_dealt_amount += 1
+	update_game.emit()
 	get_node("UI/PostDealt").set("visible", true)
 
 func next_deal_player() -> void:
 	card_value = deal_card()
 	if g.player_dealt_amount == 2 :
-		set_card3.emit(card_value)
+		g.player_cards[2] = card_value
 		g.player_dealt_amount += 1
 	elif g.player_dealt_amount == 3 :
-		set_card4.emit(card_value)
+		g.player_cards[3] = card_value
 		g.player_dealt_amount += 1
 	elif g.player_dealt_amount == 4 :
-		set_card5.emit(card_value)
+		g.player_cards[4] = card_value
 		g.player_dealt_amount += 1
+	update_game.emit()
 
 func next_deal_opponent() -> void:
 	card_value = deal_card()
-	if g.player_dealt_amount == 2 :
-		set_card3.emit(card_value)
-		g.player_dealt_amount += 1
-	elif g.player_dealt_amount == 3 :
-		set_card4.emit(card_value)
-		g.player_dealt_amount += 1
-	elif g.player_dealt_amount == 4 :
-		set_card5.emit(card_value)
-		g.player_dealt_amount += 1
+	if g.opponent_dealt_amount == 2 :
+		g.opponent_cards[2] = card_value
+		g.opponent_dealt_amount += 1
+	elif g.opponent_dealt_amount == 3 :
+		g.opponent_cards[3] = card_value
+		g.opponent_dealt_amount += 1
+	elif g.opponent_dealt_amount == 4 :
+		g.opponent_cards[4] = card_value
+		g.opponent_dealt_amount += 1
+	update_game.emit()
 
 func _on_hit_pressed() -> void:
 	next_deal_player()
 	print("hit pressed")
 
 func _on_stand_pressed() -> void:
-	get_node("UI/PostDealt").set("visible", false)
 	print("stand pressed")
+	get_node("UI/PostDealt").set("visible", false)
+	for x in range(3) :
+		if opponent_decide() == true:
+			next_deal_opponent()
+		else:
+			break
+
+	if g.opponent_dealt_amount >= 5 or g.opponent_score > 21:
+		if g.opponent_dealt_amount >= 5:
+			g.win_reason = "Higher score wins."
+			opponent_wins()
+		elif g.opponent_score > 21:
+			g.win_reason = "Went over 21."
+			player_wins()
+	elif g.player_score > g.opponent_score:
+		g.win_reason = "Higher score wins."
+		player_wins()
+	elif g.opponent_score > g.player_score:
+		g.win_reason = "Higher score wins."
+		opponent_wins()
 
 func _on_deal_pressed() -> void:
 	initial_deal_player()
+	initial_deal_opponent()
 	get_node("UI/PreDealt").set("visible", false)
+
+func _on_update_game() -> void:
+	print("score update")
+	g.player_score = 0
+	g.opponent_score = 0
+	for num in g.player_cards :
+		g.player_score += num
+	for num in g.opponent_cards :
+		g.opponent_score += num
+	get_node("UI/Text/YouScore").set("text", g.player_score)
+	get_node("UI/Text/VSScore").set("text", g.opponent_score)
+	if g.player_dealt_amount >= 5 or g.player_score > 21:
+		if g.player_dealt_amount >= 5:
+			g.win_reason = "Higher score wins."
+			player_wins()
+		elif g.player_score > 21:
+			g.win_reason = "Went over 21."
+			opponent_wins()
+
+func next_round() -> void:
+	g.player_cards = [0,0,0,0,0]
+	g.player_dealt_amount = 0
+	g.opponent_cards = [0,0,0,0,0]
+	g.opponent_dealt_amount = 0
+	g.player_score = 0
+	g.opponent_score = 0
+	g.round += 1
+	get_node("UI/Text/RoundNo").set("text", g.round)
+	get_node("Winner").set("visible", false)
+	get_node("Winner/PlayerWin").set("visible", false)
+	get_node("Winner/OpponentWin").set("visible", false)
+
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_CTRL) and Input.is_key_pressed(KEY_ALT) and Input.is_key_pressed(KEY_D) :
+		if globals.dev_mode == false :
+			print("DEV MODE ENABLED")
+			dev = true
+			dev_mode_enabled.emit()
+
+func opponent_decide() -> bool:
+	if g.opponent_score >= 21 :
+		return false
+	else :
+		var decision_decider = randf()
+		var decision_chance = opponent_decision_chances[g.opponent_score - 1]
+		if decision_decider < decision_chance :
+			return true
+	return false
+
+func player_wins() -> void:
+	print("player win")
+	get_node("UI/PreDealt").set("visible", false)
+	get_node("Winner/WinReason").set("text", g.win_reason)
+	get_node("Winner").set("visible", true)
+	get_node("Winner/PlayerWin").set("visible", true)
+
+func opponent_wins() -> void:
+	print("opponent win")
+	get_node("UI/PreDealt").set("visible", false)
+	get_node("Winner/WinReason").set("text", g.win_reason)
+	get_node("Winner").set("visible", true)
+	get_node("Winner/OpponentWin").set("visible", true)
+
+func _on_play_again_pressed() -> void:
+	next_round()
